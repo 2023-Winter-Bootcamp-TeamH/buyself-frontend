e0bd9b232047cf6fa47d15773bcba524e97e6849
권광재 <kgj9906@naver.com>
2023-01-26 03:15:43 +0900

    feat:paymentFail

---
+++ src/components/PaymentFail/PaymentFailButton.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+
+/** 결제실패페이지 이동버튼
+ * 이전 페이지로
+ */
+
+const PaymentFailButton = ({
+  title,
+  IsRed,
+}: {
+  title: string
+  IsRed?: boolean
+}) => {
+  if (IsRed) {
+    return <RedBox>{title}</RedBox>
+  } else {
+    return <Box>{title}</Box>
+  }
+}
+export default PaymentFailButton
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 12rem;
+  height: 3rem;
+  margin: 1.25rem 1.5rem 1.25rem 1.5rem;
+  box-shadow: 0.25rem 0.28rem black;
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: black;
+  background: #95DCEB;
+  border: 1px solid black;
+  border-radius: 0.7rem;
+  @media all and (max-width: 767px) {
+    width: 8.5rem;
+    height: 2.5rem;
+    margin: 1.25rem 0.5rem 1.25rem 0.5rem;
+    font-size: 1rem;
+  }
+  &:hover {
+    background-color: #cae7ed;;
+`
+
+const RedBox = styled(Box)`
+  background-color: #ff6060;
+  &:hover {
+    background-color: #ff9292;
+  }
+`
