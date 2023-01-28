9d9444c9aec9387d62a9da02e3c2596f04a27b61
rabbit-22 <chlwlal20@gmail.com>
2023-01-29 03:39:29 +0900

    style: main page image

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -21,6 +21,10 @@ const MainPageLayout = styled.div`
   justify-content: space-between;
   width: 100vw;
   height: 100vh;
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 `
 
 const Logo = styled.img`
