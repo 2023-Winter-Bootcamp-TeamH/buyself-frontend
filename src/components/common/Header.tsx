28e38f09d19017ef219db461c86c8c6dc456ec07
rabbit-22 <chlwlal20@gmail.com>
2023-01-29 03:36:17 +0900

    feat: header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -61,17 +61,24 @@ const Box = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 0 1.5rem;
-  height: 5rem;
+  height: 6.8rem;
+  mask: radial-gradient(
+        17.09px at 50% calc(100% - 23.8px),
+        #000 99%,
+        #0000 101%
+      )
+      calc(50% - 28px) 0/56px 100%,
+    radial-gradient(17.09px at 50% calc(100% + 9.8px), #0000 99%, #000 101%) 50%
+      calc(100% - 14px) / 56px 100% repeat-x;
   background: rgb(255, 255, 255);
-  box-shadow: 0rem 0.2rem 0.8rem rgba(0, 0, 0, 0.2);
   @media all and (max-width: 767px) {
-    height: 5rem;
+    height: 6rem;
   }
 `
 const Image = styled.img`
   display: flex;
   align-items: center;
-  width: 10rem;
+  width: 11.875rem;
   @media all and (max-width: 767px) {
     width: 9rem;
     margin-top: 0.1rem;
@@ -81,8 +88,8 @@ const Image = styled.img`
 const Arrow = styled.label`
   display: flex;
   align-items: center;
-  font-size: 2.2rem;
-  margin-top: 0.2rem;
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
   cursor: pointer;
   @media all and (max-width: 767px) {
     font-size: 2.2rem;
@@ -94,11 +101,12 @@ const Cart = styled.label`
   display: block;
   display: flex;
   align-items: center;
-  font-size: 2.4rem;
-  margin-top: 0.2rem;
+  font-size: 2.8rem;
+  margin-bottom: 1.5rem;
   cursor: pointer;
   @media all and (max-width: 767px) {
     font-size: 2.5rem;
+    margin-bottom: 1rem;
   }
 `
 
