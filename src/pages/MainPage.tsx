eba58f2699273a2d2c391f6b0023c0752ebe20de
eomseona <eomsa98@gmail.com>
2023-01-29 07:46:13 +0900

    error: background change error

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,27 +2,36 @@ import React from 'react'
 import styled from 'styled-components'
 import '../components/main/MainPage.css'
 import $ from 'jquery'
+import logo from '../images/header_logo.svg'
+import MainContainer from '../components/main/MainContainer'
 
 const MainPage = () => {
-  const listHTML = $('.Title').html()
-  const listItems = listHTML.split('<br>')
-  $('.Title').html('')
-  $.each(listItems, function (i, v) {
-    const item =
-      '<div class="Title-mask"><span class="Title-line">' + v + '</span></div>'
-    $('.Title').append(item)
-  })
+  const text = ['buyself', 'Buy products', 'just by yourself']
+  const list = []
   return (
-    <h1 data-linecount="3" className="Title">
-      The spectacle
-      <br> before us was</br>
-      <br>
-        indeed
-        <span className="Title-cursive">sublime</span>
-        <span className="Title-dot">.</span>
-      </br>
-    </h1>
+    <div className="bo">
+      <div className="Title">
+        {text.map((i: any, v: any) => (
+          <div className="ac" key="i">
+            {i}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
 export default MainPage
+
+const Logo = styled.img`
+  display: flex;
+  width: 20rem;
+  align-self: center;
+  margin-top: 2rem;
+  @media all and (min-width: 768px) and (max-width: 1023px) {
+    width: 15rem;
+  }
+  @media all and (max-width: 767px) {
+    width: 14rem;
+  }
+`
